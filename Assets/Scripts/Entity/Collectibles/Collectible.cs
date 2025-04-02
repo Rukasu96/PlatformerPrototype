101951463bba8997e8da240d259d5c243d4e5bbc
Rukasu96 <lukaszkulesza@op.pl>
2025-04-02 15:05:55 +0200

    Added collecting effect

--- Assets/Scripts/Entity/Collectibles/Collectible.cs
+++ Assets/Scripts/Entity/Collectibles/Collectible.cs
@@ -1,6 +1,31 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Collectible : Entity
 {
-    
+    [SerializeField] float collectAnimationOnY;
+    [SerializeField] float jumpDuration;
+    [SerializeField] float scaleAnimation;
+    [SerializeField] float scaleDuration;
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        Collected();
+    }
+
+    protected void Collected()
+    {
+        transform.DOMoveY(collectAnimationOnY, jumpDuration).OnComplete(() =>
+        {
+            transform.DOScale(scaleAnimation, scaleDuration).OnComplete(() =>
+            {
+                Destroy(gameObject);
+            });
+        });
+    }
 }
