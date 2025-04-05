252439839e4c85b3bf413850aca4b796cb2558f4
Rukasu96 <lukaszkulesza@op.pl>
2025-04-05 09:40:15 +0200

    Destroying boxes

--- Assets/Scripts/Entity/Boxes/Box.cs
+++ Assets/Scripts/Entity/Boxes/Box.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public abstract class Box : Entity
 {
+    protected abstract void OnTriggerStay(Collider other);
+
     protected abstract void OnTriggerEnter(Collider other);
 
     protected abstract void BoxDestroy();
--- Assets/Scripts/Entity/Boxes/CollectibleBox.cs
+++ Assets/Scripts/Entity/Boxes/CollectibleBox.cs
@@ -1,9 +1,11 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollectibleBox : Box
 {
     [SerializeField] CollectibleBoxData collectibleBoxdata;
+    [SerializeField] Transform topHitPoint;
     
     public static event Action<CollectibleData, Transform, int> OnCollectibleBoxDestroyed;
 
@@ -13,13 +15,25 @@ public class CollectibleBox : Box
         Destroy(gameObject);
     }
 
-    protected override void OnTriggerEnter(Collider other)
+    protected override void OnTriggerStay(Collider other)
     {
-        if (!other.gameObject.CompareTag("Player"))
+        if (!other.GetComponent<Movement>().isAttack)
         {
             return;
         }
+        BoxDestroy();
+    }
 
+    protected override void OnTriggerEnter(Collider other)
+    {
+        Vector3 distance = other.gameObject.transform.position - topHitPoint.transform.position;
+        float distanceY = Mathf.Abs(distance.y);
+
+        if(distanceY < 0.7)
+        {
+            return;
+        }
+        other.GetComponent<Movement>().Bounce();
         BoxDestroy();
     }
 }
